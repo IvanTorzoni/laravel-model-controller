bcfdfc428d5bd4a67abcba505e9c623a304c6740
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-10 14:13:55 +0200

    feat: printed card in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home page</h1>
+    <section>
+        <div class="container">
+            <div class="row g-3">
+                @foreach ($movies as $movie)
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="ms_card h-100">
+                            <img src="{{ $movie['image'] }}" alt="">
+                            <div class="ms_card-body">
+                                <p>
+                                    {{ $movie['name'] }}
+                                </p>
+                                <p>
+                                    {{ $movie['original_title'] }}
+                                </p>
+                                <p>
+                                    {{ $movie['nationality'] }}
+                                </p>
+                                <p>
+                                    {{ $movie['date'] }}
+                                </p>
+                                <p>
+                                    {{ $movie['vote'] }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
